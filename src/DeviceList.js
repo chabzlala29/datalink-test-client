d094f123d96219d130cf04a995713b347e324c6d
Charles Darwin D.C. Pobre <cpobre.peg@gmail.com>
2017-05-13 00:14:37 +0800

    initial react app

---
+++ src/DeviceList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DeviceList.css';
+
+class DeviceList extends Component {
+  render() {
+    return (
+      <div className="DeviceList">
+        <h1>Device List Page</h1>
+      </div>
+    );
+  }
+}
+
+export default DeviceList;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DeviceList from './DeviceList';
+import './index.css';
+
+ReactDOM.render(
+  <DeviceList />,
+  document.getElementById('device-list')
+);
