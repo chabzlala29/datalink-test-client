31614873f5473a21240a05c66124eec6cdd8fdc0
Charles Darwin D.C. Pobre <cpobre.peg@gmail.com>
2017-05-13 00:49:24 +0800

    update constructor

--- src/DeviceList.js
+++ src/DeviceList.js
@@ -2,47 +2,54 @@ import React, { Component } from 'react';
 import DeviceRow from './DeviceRow';
 import './DeviceList.css';
 
-const columnNames = [
-  'ID',
-  'Device Mac Address',
-  'Serial #',
-  'Color',
-  'Name',
-  'Description',
-  'Building',
-  'Level',
-  'Device Threshold',
-  'Created At',
-  'Updated At'
-];
 
 class DeviceList extends Component {
-  render() {
-    let items = [{
-      id: 1,
-      device_mac: '13123TESTBLA',
-      serial_no: 'BLASER12',
-      color: 'RED',
-      name: 'My Name',
-      description: 'Description',
-      building: 'Building',
-      level: 'GRD FLR',
-      device_threshold: 9,
-      created_at: "10/20/2019",
-      updated_at: "10/20/2019"
-    }];
+  constructor() {
+    super();
 
+    this.items = [
+      {
+        id: 1,
+        device_mac: '13123TESTBLA',
+        serial_no: 'BLASER12',
+        color: 'RED',
+        name: 'My Name',
+        description: 'Description',
+        building: 'Building',
+        level: 'GRD FLR',
+        device_threshold: 9,
+        created_at: "10/20/2019",
+        updated_at: "10/20/2019"
+      }
+    ];
+
+    this.columnNames = [
+      'ID',
+      'Device Mac Address',
+      'Serial #',
+      'Color',
+      'Name',
+      'Description',
+      'Building',
+      'Level',
+      'Device Threshold',
+      'Created At',
+      'Updated At'
+    ];
+  }
+
+  render() {
     return (
       <div className="DeviceList">
         <div className="container">
           <table className="table table-striped">
             <thead>
               <tr>
-                {columnNames.map( (column, index) => <th key={index}>{column}</th> )}
+                {this.columnNames.map( (column, index) => <th key={index}>{column}</th> )}
               </tr>
             </thead>
             <tbody>
-              {items.map( item => <DeviceRow item={item} key={item.id} /> )}
+              {this.items.map( item => <DeviceRow item={item} key={item.id} /> )}
             </tbody>
           </table>
         </div>
